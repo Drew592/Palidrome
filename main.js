3f26a7e41b87f4739a19e6ec184e8c3e0a8432da
Cornmog <drew.rosa24@student.cart.org>
2025-04-09 13:59:08 -0700

    update.main.js

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ function backwardsWord(word) {
 let yourWord = prompt ("Type in your chosen word")
 let backwardWord = backwardsWord(yourWord)
 if (yourWord == backwardWord){
-    console.log (yourWord + " Your word is a plaindrome")
+    console.log (yourWord + " True ")
 }
 else if (yourWord!= backwardWord){
-    console.log(yourWord + " Your word is not a plindrome ya failed")
+    console.log(yourWord + " False ")
 }
